5c9ec0f04e5199a8857c9e2560bf71e63ddfaccf
aryamanmajumdar <aryaman.cis@gmail.com>
2021-07-03 04:39:34 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 import '../css/main.css';
 
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
 
 // Setup
 
